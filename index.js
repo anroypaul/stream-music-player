21aa9be5696777dc60f07dd53df3782abb2f35ea
Paul K <anroypaul@gmail.com>
2021-03-26 19:47:34 -0700

    change logic/update client

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ const express = require("express");
 const axios = require("axios");
 const parseString = require("xml2js").parseString;
 const cors = require("cors");
+const http = require("http");
+const request = require("request");
 
-const app = express();
 const port = process.env.PORT || 5000;
 
+const API_STREAM_LINK = "http://air.aristocrats.fm:8000/live2";
+
+const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(express.static("public"));
@@ -28,6 +32,30 @@ app.get("/current-track", async (req, res) => {
     });
 
   //   res.sendStatus(200);
+});
+
+app.get("/audio-stream", (req, res) => {
+  // await axios
+  //   .get(API_STREAM_LINK, {
+  //     responseType: "arraybuffer",
+  //     headers: {
+  //       "Content-Type": "audio/mpeg",
+  //     },
+  //   })
+  //   .then((response) => {
+  //     response.data.pipe(res);
+  //   });
+  let streamUrl = API_STREAM_LINK;
+
+  request.get(API_STREAM_LINK, (error, resp) => {
+    console.log(resp);
+    if (resp.statusCode == 302) {
+      streamUrl = resp.headers.location;
+    }
+  });
+
+  res.set("Content-Type", "audio/mpeg");
+  req.pipe(request.get(streamUrl)).pipe(res);
 });
 
 app.listen(port, () => {
